6f13c83aa3e3275e95f793ddc04948fc1975c654
victoria-yining-huang <victoria.huang777@gmail.com>
2024-11-05 23:59:12 -0500

    comment

--- src/sentry/utils/event_tracker.py
+++ src/sentry/utils/event_tracker.py
@@ -31,6 +31,8 @@ logger = logging.getLogger("EventTracker")
 def is_sampled_to_track(event_id: str, sample_rate: float) -> bool:
     """
     Normalize the integer to a float in the range [0, 1)
+    The md5 hashing algorithm is consistent and will make sure the same event_id's are sampled,
+    and provide all or nothing logging.
     """
     hash_float = int(md5_text(event_id).hexdigest(), 16) / (2**128 - 1)
     if hash_float < sample_rate:
